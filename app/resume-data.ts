86bab0d735b1d2ce821089149cfeec9ffb81ed2c
liaoming <liaoming@iaskdoc.com>
2018-05-28 23:08:49 +0800

    update

--- app/resume-data.ts
+++ app/resume-data.ts
@@ -15,7 +15,7 @@ export var PROFILE: Profile = {
     "weixin": "",
     "phone": "15626139118",
     "nickName": "xqMing",
-    "applyJob": "java开发工程师",
+    "applyJob": "Java研发工程师",
     "userBaseInfo": "1993年9月23日/男",
     "educationInfo": "本科/广财大学信息学院/2016年毕业",
     "githubUrl": "https://github.com/QiuMing",
@@ -34,17 +34,17 @@ export var SKILLS: Skill[] = [
             {
                 "name": "语言",
                 "descriptions": [
-                    "熟悉Java流行技术栈：spring-boot、mybatis、shiro、spring data jpa、elasticsearch， dubbo 框架，分布式配置 disconf，分布式作业 elastic job，java 爬虫框架 webmagic。",
-                    "能够使用过dubbo、zookeeper 搭建 SOA 应用。",
-                    "了解 Spring Cloud 微服务技术栈，了解 RxJava，jersey。"
+                    "熟悉Java流行技术栈：Spring Boot、Mybatis、Shiro、Spring Data Dpa、Elasticsearch、Dubbo、Disconf、Elastic-Job 以及爬虫框架 Webmagic。",
+                    "能够使用Dubbo、Zookeeper 构建微服务应用，了解 Spring Cloud 微服务技术栈，了解 RxJava，Jersey。",
+                    "会用 Python 编写简单的脚本以及爬虫，辅助日常的开发工作。"
                 ]
             }, {
                 "name": "系统工具",
                 "descriptions": [
-                    "熟悉 Linux 操作，日常 Linux 办公半年，使用过腾讯|阿里云。",
-                    "熟悉 eclipse、idea 、Navicat Premium、git 等工具。",
-                    "熟悉 mysql 、redis、mongodb。",
-                    "了解 docker 常用命令，可以使用docker 构建发布镜像。"
+                    "熟悉 Linux 操作，日常 Linux 办公半年，使用过腾讯云和阿里云。",
+                    "熟悉 Eclipse、Idea 、Navicat Premium、Git 等工具。",
+                    "熟悉 Mysql 、Redis、Mongodb。",
+                    "了解 Docker 常用命令，会用Docker 构建镜像。"
                 ]
             } 
         ]
@@ -55,9 +55,9 @@ export var SKILLS: Skill[] = [
                 "name": "HTML / CSS / JS",
                 "descriptions": [
                     "能够编写语义化的 HTML，模块化的 CSS，完成较简单的布局。",
-                    "熟悉bootstrap，能够使用它编写简单的应用，能够驾驭常见 bootstrap 后台模板。",
-                    "会使用 vuejs,并能够使用它们编写简单的应用。",
-                    "了解 Yeoman 、Bower、 Grunt 、Gulp、webpack 前端自动化工具。",
+                    "熟悉Bootstrap，能够编写简单的应用，能够驾驭常见 Bootstrap 后台模板。",
+                    "使用过 Vue.js，以及 Element UI，并能够编写简单的后台应用。",
+                    "了解 Yeoman 、Bower、 Grunt 、Gulp、Webpack 前端自动化工具。",
                 ]
             }  
         ]
@@ -85,8 +85,8 @@ export var PERSONALPROJECTS: PersonalProject[] = [
 		"name": "JHipster 中文文档翻译与维护",
 		"time": "",
 		"descriptions": [
-			"JHipster 是一个国外开源，集 JavaEE 最佳实践与代码生成于一体到开源项目。",
-            "本人组织并参与了 JHipster 官方文档到翻译，详见：https://github.com/jhipster-cn。",
+			"JHipster 是一个国外开源，集 JavaEE 最佳实践，以及代码生成于一体的开源项目。",
+            "本人组织并参与了 JHipster 官方文档的翻译，详见：https://github.com/jhipster-cn。",
         ],
 		"images": [],
 		"codeUrl": "https://github.com/jhipster-cn",
@@ -95,7 +95,7 @@ export var PERSONALPROJECTS: PersonalProject[] = [
 		"name": "知乎爬虫",
 		"time": "",
 		"descriptions": [
-			"大学时学习爬虫编写，使用到webmagic、spring-boot、redis 等技术，使用echart做数据可视化。"
+			"在校学习爬虫时编写，使用到Webmagic、Spring Boot、Redis等技术，使用Echart做数据可视化。"
 		],
 		"images": [
 			"img/zhihu2.png",
@@ -106,15 +106,15 @@ export var PERSONALPROJECTS: PersonalProject[] = [
 ]
 
 export var COMPANYPROJECT: CompanyProject[] = [
-	{
-	   "companyName": "cooka 网络科技有限公司(2015.1 - 2016.5)",
+    {
+	   "companyName": "广州市贝聊信息科技有限公司(2017.5 - 至今)",
 	    "time": "",
 		"project": [
             {
-                "name": "cooka B2B跨境商场系统 PC | 微信 商城服务端开发",
+                "name": "支撑组基础研发工作",
                 "descriptions": [
-                    "全程参与负责了系统用户、购物车、风控、运费、权限、统计、安全中心几个模块的后台设计与开发。",
-                    "涉及技术栈：spring boot、mybatis、shiro、velocity、redis、fastdfs。"
+                    "参与 Api 接口、底层Dubbo服务重构，及日常版本迭代开发工作；参与服务端VPC迁移；独立负责盈利的 SaaS 业务服务端工作；负责招生小程序接口开发、运营抽奖活动接口开发；引入Spring Boot 框架，负责部分新项目后台基础框架搭建。",
+                    "涉及技术栈：Spring Boot，Dubbo，Disconf，Elastic-Job，Redis，Cat ，ELK 监控等 。"
                 ],
                 "images": [
                     "img/cooka1.png",
@@ -124,27 +124,29 @@ export var COMPANYPROJECT: CompanyProject[] = [
                 "demoUrl": ""
             } 
         ]
-	},{
+	},
+    {
 	   "companyName": "雅特网络科技有限公司(2016.5 - 2917.5)",
 	    "time": "",
 		"project": [
             {
-                "name": "叮叮医生 APP | 微信 | PC端 后台系统维护与开发",
+                "name": "叮叮医生 APP | 微信 | PC端后台系统维护与开发",
                 "descriptions": [
-                    "负责系统的日常维护，新版本迭代开发，涉及微信支付，聊天，搜索等模块。",
-                    "涉及技术栈：spring boot（在内部分享后，在新项目引入） 、mybatis、mongodb、redis 集群、阿里云mq 和 oss、elasticsearch、rpc 用 rmi、Quartz、环信 IM、vue.js。"
+                    "负责叮叮医生 App 后台系统，以及Api 接口的日常维护与版本迭代开发工作，涉及微信支付，聊天，搜索，IM 等核心模块。",
+                    "涉及技术栈：Spring Boot（内部分享后，在新项目引入） 、Mybatis、Mongodb、Redis 集群、阿里云MQ和OSS、Elasticsearch、RPC 用 RMI、Quartz、环信 IM、Vue.js。"
                 ]
             }
         ]
-	},{
-	   "companyName": "广州市贝聊信息科技有限公司(2017.5 - 至今)",
+	},
+	{
+	   "companyName": "Cooka 网络科技有限公司(2015.1 - 2016.5)",
 	    "time": "",
 		"project": [
             {
-                "name": "支撑组基础研发工作",
+                "name": "Cooka B2B跨境商场系统 PC | 微信商城服务端开发",
                 "descriptions": [
-                    "参与 app 端接口重构，日常功能迭代开发工作；独立负责服务端 SaaS 核心业务工作；负责招生小程序接口开发、运营抽奖活动接口开发。",
-                    "涉及技术栈：spring-boot（在内部分享后，在新项目引入） ，dubbo，disconf，elastic-job，redis，cat ,elk 等 。"
+                    "项目从零开始，我参与负责了系统用户、购物车、运费、权限、统计、安全中心几个模块的后台设计与开发。",
+                    "涉及技术栈：Spring Boot、Mybatis、Shiro、Velocity、Redis、Fastdfs。"
                 ],
                 "images": [
                     "img/cooka1.png",
@@ -158,8 +160,8 @@ export var COMPANYPROJECT: CompanyProject[] = [
 ]
 
 export var MAINDES: string[] = [
-    "专注于服务端开发 3.5年，熟悉 java，爱折腾，爱开源。",
-    "熟悉电商系统、app、微信后台开发,，能够独立完成中小型到系统应用架构，能够使用 dubbo 搭建 SOA 架构应用，了解 Spring Cloud 微服务技术栈"。,
+    "专注于服务端开发 3.5年，熟悉 Java，技术功底扎实，读过 Spring 源码，喜欢开源。",
+    "熟悉电商系统、App、微信后台开发，能够独立完成中小型的系统搭建，能够使用 Dubbo 搭建微服务应用，了解 Spring Cloud技术栈。",
     "未来想成为一个架构师，目前在努力加深拓宽自己的技术栈，渴望结识更多志同道合到人。",
 ]
 
